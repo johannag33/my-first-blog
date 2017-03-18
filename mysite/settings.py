29633fb9c5837c6d4477748c7bdb2d8e5d1bf006
Johanna <johanna.guilhemjouan@gmail.com>
2017-03-18 15:18:19 +0100

    Modif du HTML

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%3drmiwlyk2+vp@dyyeib457(832im1tx8t6jz0x3c619f%tyu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["johannag33.pythonanywhere.com"]
+ALLOWED_HOSTS = ["johannag33.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
